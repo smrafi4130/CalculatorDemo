92967916a4012f0f3d4f8777e0d279b70fc15c58
Smrafi <smrafi4130@gmail.com>
2017-10-21 03:38:54 +0600

    Changed

---
+++ gen/smrafi/com/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package smrafi.com;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/smrafi/com/Activity_Main.java
@@ -0,0 +1,13 @@
+package smrafi.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity_Main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
